dff5c7106717aae9303d0d64a511aac58d1a1567
A.Rahman Mirza <48047974+AbdurRahmanMirza@users.noreply.github.com>
2024-03-18 17:00:39 +0500

    Linear System Iteration

---
+++ linear_system_iteration/jacobi.m
@@ -0,0 +1,24 @@
+function X = jacobi(A,b,P,tol,max_itr)
+%{
+Jacobi Iteration
+Input:
+      A is n-by-n non-singualr matrix
+      b is column vector
+      P is column vector of initail guess
+      tol is the tolarance of 'P'
+      max_itr is the maximum number of iterations
+Output:
+      X is column vector, the jacobi approximation
+      to the solution of AX = b 
+%}
+
+n = length(b);
+for  i = 1:max_itr
+    for j = 1:n
+        X(j) = (b(j)-A(j,[1:j-1,j+1:n])*P([1:j-1,j+1:n]))/A(j,j);
+    end
+    err = abs(norm(X'-P));
+    rel_err = err/(norm(X)+eps);
+    P = X';
+    if (err < tol) || (rel_err < tol), break, end
+end
