6e297e5b28a77628633f57f4ec84b4dde967f303
A.Rahman Mirza <48047974+AbdurRahmanMirza@users.noreply.github.com>
2024-03-18 17:09:58 +0500

    Update Simpson3by8.m

--- integration/Simpson3by8.m
+++ integration/Simpson3by8.m
@@ -1,18 +1,17 @@
 function I = Simpson3by8(func,a,b,n)
 %{
-Simpson's 3/8 Rule
+Composite Simpson's 3/8 Rule
 Input:
     func = handle of function to be integrated
     a = lower bound
     b = upper bound
-    n = number of intervals, defaults to 100, must be even
+    n = number of intervals
 Output:
     I = result of integration
 %} 
 
-if nargin < 3, error('insufficient inputs'); end
+if nargin < 4, error('insufficient inputs'); end
 if a > b, error('b must be greater than a'); end
-if nargin < 4, n = 100; end
 if mod(n,3) == 1, error('n must be multiple of 3'); end
 
 x = zeros(1,n-1); y = zeros(1,n-1);
