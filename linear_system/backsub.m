d286100d9261f2f4ad68f9e37f9665bb19454d69
A.Rahman Mirza <48047974+AbdurRahmanMirza@users.noreply.github.com>
2024-03-18 14:44:12 +0500

    linear system solutions

---
+++ linear_system/backsub.m
@@ -0,0 +1,19 @@
+function X = backsub(A,b)
+%{ 
+Back-Subtituion
+Input:
+    A = upper triangular matrix
+    b = column vector
+Output: 
+    X = column vecotr
+%} 
+[m,n] = size(A);
+if m~=n, error("The input matrix A isn't a square matrix"), end
+
+n = length(b);
+X = zeros(n,1);
+X(n) = b(n)/A(n,n);
+
+for i = n-1:-1:1
+    X(i) = (b(i) - A(i,i+1:n)*X(i+1:n))/A(i,i);
+end
---
+++ linear_system/forsub.m
@@ -0,0 +1,19 @@
+function Y = forsub(A,b)
+%{
+Forward Substituion
+Input:
+    A = lower triangular matrix
+    b = column vector
+Output: 
+    Y = column vecotr
+%}
+[m,n] = size(A);
+if m~=n, error("The input matrix A isn't a square matrix"), end
+
+n = length(b);
+Y = zeroes(n,1);
+Y(1) = b(1)/A(1,1);
+
+for i = 2:1:n
+    Y(i) = (b(i)-A(i,1:i-1)*Y(1:i-1))/A(i,i);
+end
