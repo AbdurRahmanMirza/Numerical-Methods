a2edeef1e8578bed5cdbf5d87b730761b4c7d368
A.Rahman Mirza <48047974+AbdurRahmanMirza@users.noreply.github.com>
2024-03-18 17:08:58 +0500

    update Simpson3by8.m

--- integration/Simpson3by8.m
+++ integration/Simpson3by8.m
@@ -1,6 +1,6 @@
 function I = Simpson3by8(func,a,b,n)
 %{
-Simpson's 1/3 Rule
+Simpson's 3/8 Rule
 Input:
     func = handle of function to be integrated
     a = lower bound
@@ -13,19 +13,19 @@ Output:
 if nargin < 3, error('insufficient inputs'); end
 if a > b, error('b must be greater than a'); end
 if nargin < 4, n = 100; end
-if mod(n,2) == 1, error('n must be even'); end
+if mod(n,3) == 1, error('n must be multiple of 3'); end
 
 x = zeros(1,n-1); y = zeros(1,n-1);
-sum_odd = 0; sum_even = 0;
+sum_3 = 0; sum_other = 0;
 h = (b-a)/n; 
 
 for i = 1:1:n-1
     x(i) =  a + i*h;
     y(i) = func(x(i));
-    if mod(i,2) == 1
-        sum_odd = sum_odd + y(i);
+    if mod(i,3) == 0
+        sum_3 = sum_3 + y(i);
     else
-        sum_even = sum_even + y(i);
+        sum_other = sum_other + y(i);
     end 
 end
-I = (3*h/8)*(func(a)+func(b)+4*sum_odd+2*sum_even);
+I = (3*h/8)*(func(a) + func(b) + 2*sum_3 + 3*sum_other);
