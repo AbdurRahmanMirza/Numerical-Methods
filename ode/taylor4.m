28fd7f52e640439dc21cf48cdffb11bb7091268e
A.Rahman Mirza <48047974+AbdurRahmanMirza@users.noreply.github.com>
2024-03-19 10:52:33 +0500

    Update taylor4.m

--- ode/taylor4.m
+++ ode/taylor4.m
@@ -3,13 +3,13 @@ function [vt,vy] = taylor4(func,dfunc1,dfunc2,dfunc3,a,b,n,y_initial)
 Taylor's (Order Four) Method
 Input:
     func = function f(t,y)
-    dfunc1 = first derevative of f(t,y)
-    dfunc2 = second derevative of f(t,y)
-    dfunc3 = third derevative of f(t,y)
+    dfunc1 = first derivative of f(t,y)
+    dfunc2 = second derivative of f(t,y)
+    dfunc3 = third derivative of f(t,y)
     a = initial point
     b = final point
     n = number of intervals  
-    y_intial = initial value of dependent variable
+    y_intial = initial value of the dependent variable
      
 Output:
     vt = values of t
