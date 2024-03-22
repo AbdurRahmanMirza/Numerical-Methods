77dc0f950128190a6ba89f827317d9f1f6bbdae9
A.Rahman Mirza <48047974+AbdurRahmanMirza@users.noreply.github.com>
2024-03-22 16:03:22 +0500

    update

---
+++ linear_system/cramer.m
@@ -0,0 +1,23 @@
+function x = cramer(A,b)
+%{
+Cramer's Rule
+Input: 
+      A = n-by-n coefficient matrix
+      b = n-by-1 source matrix
+Output:
+      x = n-by-1 matrix, solution of Ax = b
+%}
+
+ n = length(b);
+ x =zeros(n,1);
+ Aold = A; d = det(A);
+ if d ~= 0
+     for i = 1:n
+         A(:,i) = b;
+         x(i) = det(A)/d;
+         A = Aold;
+     end
+ else
+     error("Cramer's rule can't be applied.")
+ end
+end
